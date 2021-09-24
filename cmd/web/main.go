fdcc3c2dd09d427a799790f44e8fbd425c6aab96
ampaking <78628177+ampaking@users.noreply.github.com>
2021-09-24 22:04:50 +0900

    add template

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/RuqayyaZaheer/basic-web-app/pkg/config"
 	"github.com/RuqayyaZaheer/basic-web-app/pkg/handlers"
+	"github.com/RuqayyaZaheer/basic-web-app/pkg/render"
 )
 
 func main() {
+
+	var app config.AppConfig
+	tc, err := render.RenderTemplateTest()
+	if err != nil {
+		log.Fatal("Can not create template cache")
+	}
+	app.TemplateCache = tc
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,20 +1,72 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
+
+var functions = template.FuncMap{}
 
 func RenderTemplate(w http.ResponseWriter, temp string) {
 
-	parsedTemplate, _ := template.ParseFiles("./templates/" + temp)
+	tc, err := RenderTemplateTest()
+	if err != nil {
+		log.Fatal(err)
+	}
+	t, ok := tc[temp]
+	if !ok {
+		log.Fatal(err)
 
-	err := parsedTemplate.Execute(w, nil)
+	}
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, nil)
+
+	_, err = buf.WriteTo(w)
 
 	if err != nil {
-		fmt.Println("error to passing template", err)
-		return
+		fmt.Println("Error writeing template to browser", err)
 	}
+
+}
+
+func RenderTemplateTest() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*page.tmpl")
+
+	if err != nil {
+		return myCache, err
+
+	}
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matchs, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+
+		}
+		if len(matchs) > 0 {
+			ts, err := ts.ParseGlob("./templates/*layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+
+			myCache[name] = ts
+		}
+
+	}
+
+	return myCache, nil
 
 }
